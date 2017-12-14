08332c4a5ca29a0850c75cb5a353caeae952ff43
MalikFaizanZafar <malikfaizanzafar1993@gmail.com>
2017-12-14 12:51:56 +0500

    first initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { Page1Page} from '../pages/page1/page1';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  Page1:any = Page1Page;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -19,4 +19,3 @@ export class MyApp {
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { Page1Page} from '../pages/page1/page1';
+import { Page2Page} from '../pages/page2/page2';
+import { Page3Page} from '../pages/page3/page3';
+import { Page4Page} from '../pages/page4/page4';
+import { Page5Page} from '../pages/page5/page5';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    Page1Page,
+    Page2Page,
+    Page3Page,
+    Page4Page,
+    Page5Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    Page1Page,
+    Page2Page,
+    Page3Page,
+    Page4Page,
+    Page5Page
   ],
   providers: [
     StatusBar,
